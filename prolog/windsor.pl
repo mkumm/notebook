0cb97e9281b3b8d20e7e537ecd34aedee305530e
Michael Kumm <michael@mkumm.com>
2023-02-20 08:41:46 +0100

    prolog section 2

---
+++ prolog/windsor.pl
@@ -0,0 +1,11 @@
+parents_of(elizabeth-ii, george-vi, elzabeth).
+parents_of(margaret, george-vi, elizabeth).
+parents_of(charles, philip, elizabeth-ii).
+parents_of(anne, philip, elizabeth-ii).
+parents_of(andrew, philip, elizabeth-ii).
+parents_of(edward, philip, elizabeth-ii).
+parents_of(william, charles, diana).
+parents_of(henry, charles, diana).
+parents_of(george, william, catherine).
+parents_of(charlotte, william, catherine).
+parents_of(louis, william, catherine).
