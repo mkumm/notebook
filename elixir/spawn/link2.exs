191efa2bd535b21282206ee5f17983da82cc6ac9
Michael Kumm <michael@mkumm.com>
2023-02-20 10:32:05 +0100

    elixir spawn examples

---
+++ elixir/spawn/link2.exs
@@ -0,0 +1,19 @@
+defmodule Link2 do
+  import :timer, only: [ sleep: 1]
+
+  def sad_function do
+    IO.inspect("sad function called")
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    spawn_link(Link2, :sad_function, [])
+    receive do
+      msg ->
+        IO.puts "Message Received: #{inspect msg}"
+      after 1000 ->
+        IO.puts "I see and saw nothing"
+    end
+  end
+end
---
+++ elixir/spawn/monitor1.exs
@@ -0,0 +1,20 @@
+defmodule Monitor1 do
+  import :timer, only: [ sleep: 1]
+
+  def sad_function do
+    IO.inspect("sad function called")
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    res = spawn_monitor(Monitor1, :sad_function, [])
+    IO.puts inspect res
+    receive do
+      msg ->
+        IO.puts "Message Received: #{inspect msg}"
+      after 1000 ->
+        IO.puts "I see and saw nothing"
+    end
+  end
+end
